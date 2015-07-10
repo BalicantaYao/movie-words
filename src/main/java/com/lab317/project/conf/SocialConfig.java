4925e55c6813ddc54285983f081d821830ed095a
redick.yao <redick.yao@ebizprise.com>
2015-07-10 16:58:35 +0800

    Add Test And Binding facebook config

--- src/main/java/com/lab317/project/conf/SocialConfig.java
+++ src/main/java/com/lab317/project/conf/SocialConfig.java
@@ -3,11 +3,10 @@
  */
 package com.lab317.project.conf;
 
-import javax.inject.Inject;
-
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
 import org.springframework.social.connect.ConnectionFactoryLocator;
 import org.springframework.social.connect.support.ConnectionFactoryRegistry;
 import org.springframework.social.facebook.connect.FacebookConnectionFactory;
@@ -16,16 +15,20 @@ import org.springframework.social.facebook.connect.FacebookConnectionFactory;
  * Created by Redick.Yao on 2015/7/8.
  */
 @Configuration
+@EnableAutoConfiguration
 public class SocialConfig {
 
-    @Inject
-    private Environment environment;
+    @Value("${facebook.app.id}")
+    public String facebookId;
+
+    @Value("${facebook.app.secret}")
+    public String facebookSecret;
 
     @Bean
     public ConnectionFactoryLocator connectionFactoryLocator() {
         ConnectionFactoryRegistry registry = new ConnectionFactoryRegistry();
-        registry.addConnectionFactory(new FacebookConnectionFactory(environment.getProperty("facebook.app.Id"),
-                environment.getProperty("facebook.app.secret")));
+        registry.addConnectionFactory(
+                new FacebookConnectionFactory(facebookId, facebookSecret));
         return registry;
     }
 }
---
+++ src/test/java/com/lab317/project/service/ParsingServiceTest.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) Lab317, Inc All Rights Reserved.
+ */
+
+package com.lab317.project.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.lab317.project.MovieWordsApplication;
+
+/**
+ * Created by Redick.Yao on 2015/7/10.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = MovieWordsApplication.class)
+public class ParsingServiceTest {
+
+    @Test
+    public void testParingService(){
+
+    }
+}
